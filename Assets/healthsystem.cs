28294b16bcb08a26b2eccafeb1223595742ee0d6
pozzitivy-show-tech <113770694+pozzitivy-show-tech@users.noreply.github.com>
2022-09-17 11:59:17 +0100

    initial commit

---
+++ Assets/healthsystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthsystem : MonoBehaviour
+{
+    public int maxhealth;
+    private float health;
+    public Image healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxhealth;
+    }
+
+    public void ChangeHealth(int value)
+    {
+        health -= value;
+
+        RenderHealth();
+    }
+
+    private void RenderHealth()
+    {
+        healthbar.fillAmount = (float)health / (float)maxhealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+	public Rigidbody2D body;
+
+	public float horizontal;
+	public float vertical;
+
+	public float runSpeed = 20.0f;
+
+	public Joystick joystick;
+
+	public SpriteRenderer spriteRenderer;
+
+	void Start ()
+	{
+		body = GetComponent<Rigidbody2D>(); 
+	}
+
+	void Update ()
+	{	
+
+		if (joystick.Horizontal >= 0.2f)
+		{
+			if( joystick.Vertical >= 0.2f)
+			{
+				horizontal = 1;
+				vertical = 1;
+			}
+			else if (joystick.Vertical <= -0.2f)
+			{
+				horizontal = 1;
+				vertical = -1;
+			}
+			else 
+			{
+				horizontal = 1;
+				vertical = 0;
+			}
+			
+		} 
+		else if (joystick.Horizontal <= -0.2f)
+		{
+			if( joystick.Vertical >= 0.2f)
+			{
+				horizontal = -1;
+				vertical = 1;
+			}
+			else if (joystick.Vertical <= -0.2f)
+			{
+				horizontal = -1;
+				vertical = -1;
+			}
+			else 
+			{
+				horizontal = -1;
+				vertical = 0;
+			}
+			
+		}
+		else
+		{
+			if( joystick.Vertical >= 0.2f)
+			{
+				horizontal = 0;
+				vertical = 1;
+			}
+			else if (joystick.Vertical <= -0.2f)
+			{
+				horizontal = 0;
+				vertical = -1;
+			}
+			else 
+			{
+				horizontal = 0;
+				vertical = 0;
+			}
+			
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		if (horizontal == 1 || horizontal == -1)
+		{
+			Vector3 charScale = transform.localScale;
+			charScale.x = Mathf.Abs(charScale.x) * horizontal;
+			transform.localScale = charScale;
+		}  
+		body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+	}
+
+}
