424f9d93d14409bc90bae8525649f78786c65a67
pozzitivy-show-tech <113770694+pozzitivy-show-tech@users.noreply.github.com>
2022-10-09 09:24:02 +0100

    some update

--- Assets/ZombieAI.cs
+++ Assets/ZombieAI.cs
@@ -6,19 +6,54 @@ using UnityEngine.AI;
 public class ZombieAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public Transform player;
+    private Transform player;
+    private string state = "idle";
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        if (player.position.x - transform.position.x < 0)
+        {
+            Debug.Log("left");
+        }
+        else if (player.position.x - transform.position.x > 0)
+        {
+            Debug.Log("right");
+        }
+
+        if (state == "idle")
+        {
+            agent.ResetPath();
+        }
+        else if (state == "attack")
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            state = "attack";
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            state = "idle";
+        }
     }
 }
